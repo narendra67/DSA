1a4faa4fec1618b0a26f76bccbfd25d9e2489bf9
narendra67 <mvnarendrareddy@gmail.com>
2019-01-18 16:26:58 +0530

    or and not gates implemented

--- misc/logic-gates.py
+++ misc/logic-gates.py
@@ -34,9 +34,9 @@ class UnaryGate(LogicGate):
 
         self.pin = None
 
-        def getPin(self):
-            return int(input("Enter Pin input for gate " + self.get_label() +
-                       "----->"))
+    def getPin(self):
+        return int(input("Enter Pin input for gate " + self.get_label() +
+                   "----->"))
 
 
 class AndGate(BinaryGate):
@@ -53,5 +53,33 @@ class AndGate(BinaryGate):
             return 0
 
 
-g1 = AndGate('G1')
-g1.get_output()
+class OrGate(BinaryGate):
+    def __init__(self, label):
+        BinaryGate.__init__(self, label)
+
+    def performGateLogic(self):
+        a = self.getPinA()
+        b = self.getPinB()
+
+        if a == 0 and b == 0:
+            return 0
+        else:
+            return 1
+
+
+class NotGate(UnaryGate):
+    def __init__(self, label):
+        UnaryGate.__init__(self, label)
+
+    def performGateLogic(self):
+        a = self.getPin()
+
+        return 0 if a == 1 else 1
+
+
+# g1 = AndGate('G1')
+# g1.get_output()
+# g2 = OrGate('O1')
+# g2.get_output()
+g3 = NotGate('N1')
+g3.get_output()
