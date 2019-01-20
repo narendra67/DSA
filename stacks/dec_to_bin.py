419e897458f0887f30a9fb1b2a65f919ef2e403b
narendra67 <mvnarendrareddy@gmail.com>
2019-01-20 23:50:33 +0530

    stacks and its problems

---
+++ stacks/dec_to_bin.py
@@ -0,0 +1,18 @@
+"""Convert a decimal number to binary number."""
+from stack import Stack
+
+
+def dec_to_bin(dec_num):
+    s = Stack()
+    bin_num = ''
+    while dec_num > 0:
+        print(dec_num, dec_num % 2)
+        s.push(dec_num % 2)
+        dec_num //= 2
+    # else:
+        # s.push(1)
+
+    while s.size():
+        bin_num += str(s.pop())
+
+    return bin_num
---
+++ stacks/stack.py
@@ -0,0 +1,36 @@
+"""Stack datastructure in python."""
+
+
+class Stack():
+    """Stack implementation using python list."""
+
+    def __init__(self):
+        self.stack = []
+
+    def is_empty(self):
+        """Check whether the stack is empty or not."""
+        return self.stack == []
+
+    def push(self, ele):
+        """Push an element into stack."""
+        self.stack.append(ele)
+
+    def pop(self):
+        """Remove/Pop top element of the stack."""
+        stack_length = len(self.stack)
+        if (stack_length) == 0:
+            return "Stack is empty, cannot perform a pop operation."
+
+        return self.stack.pop()
+
+    def size(self):
+        """Size of the stack."""
+        return len(self.stack)
+
+    def peek(self):
+        """Return top most element on the stack."""
+        stack_length = len(self.stack)
+        if (stack_length) == 0:
+            return "Stack is empty."
+
+        return self.stack[len(self.stack)-1]
