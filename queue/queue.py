0ac23f5359e91e6a9de761f96f18e6ec13987c60
narendra67 <mvnarendrareddy@gmail.com>
2019-02-10 01:02:36 +0530

    Queue using list implemented

--- queue/queue.py
+++ queue/queue.py
@@ -1,4 +1,21 @@
 """A Queue class."""
 
 class Queue():
-    """A Que"""
+    """A Queue implementation using list."""
+
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, val):
+        self.queue.append(val)
+
+    def dequeue(self):
+        return self.queue.pop(0)
+
+    def size(self):
+        """Size of the queue."""
+        return len(self.queue)
+
+    def is_empty(self):
+        """Return if the queue is empty or not."""
+        return len(self.queue) == 0
