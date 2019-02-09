3fe50b39b62754feed62edcf80157183ebe5c629
narendra67 <mvnarendrareddy@gmail.com>
2019-02-10 01:02:52 +0530

    hot potato problem implemented

---
+++ queue/hot_potato.py
@@ -0,0 +1,28 @@
+from queue import Queue
+
+
+def hot_potato(peopleList = [], num = 1):
+    """A game where a potato will be passed around n number of
+        people, until num - number at which the moving of potato
+        should be stopped and the person holding the potato will
+        be eliminated. This goes on until only one person is left.
+    """
+    tempQueue = Queue()
+
+    if len(peopleList) == 0:
+        return 'Number of participants should be atleast 1.'
+
+    for name in peopleList:
+        tempQueue.enqueue(name)
+
+    while tempQueue.size() > 1:
+        if num < 1:
+            return 'Number (num) of iterations should be >= 1.'
+        i = 1
+        while i < num:
+            tempQueue.enqueue(tempQueue.dequeue())
+            i += 1
+        else:
+            tempQueue.dequeue()
+    else:
+        return tempQueue.dequeue() + ' is the winner.'
